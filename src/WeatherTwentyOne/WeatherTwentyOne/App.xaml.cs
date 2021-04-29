e148bd576c9b4e3f5165086d3ec3c81a0aad5d38
David Ortinau <david.ortinau@microsoft.com>
2021-04-28 20:23:24 -0500

    favorites and settings roughed in

--- src/WeatherTwentyOne/WeatherTwentyOne/App.xaml.cs
+++ src/WeatherTwentyOne/WeatherTwentyOne/App.xaml.cs
@@ -10,7 +10,7 @@ namespace WeatherTwentyOne
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new MainPage() );
         }
 
         protected override void OnStart()
--- src/WeatherTwentyOne/WeatherTwentyOne/Views/NavBar_Desktop.xaml.cs
+++ src/WeatherTwentyOne/WeatherTwentyOne/Views/NavBar_Desktop.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WeatherTwentyOne.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,26 @@ namespace WeatherTwentyOne.Views
         public NavBar_Desktop()
         {
             InitializeComponent();
+        }
+
+        private void HomeTab_Clicked(object sender, EventArgs e)
+        {
+            (App.Current.MainPage as NavigationPage).PushAsync(new MainPage());
+        }
+
+        private void FavoritesTab_Clicked(object sender, EventArgs e)
+        {
+            (App.Current.MainPage as NavigationPage).PushAsync(new FavoritesPage());
+        }
+
+        private void MapTab_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SettingsTab_Clicked(object sender, EventArgs e)
+        {
+            (App.Current.MainPage as NavigationPage).PushAsync(new SettingsPage());
         }
     }
 }
