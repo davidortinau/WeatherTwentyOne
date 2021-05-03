c58c362e8ec809160ae40562ec19e3b552e7e2fa
davidortinau <david.ortinau@microsoft.com>
2021-05-03 16:01:09 -0500

    refactoring layouts
    Moved widget panel to it's own file. Adding back in FlexLayouts

---
+++ src/WeatherTwentyOne/Views/WidgetsPanel.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace WeatherTwentyOne.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WidgetsPanel
+    {
+        public WidgetsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
