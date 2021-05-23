b850610958aad90794282934a11c898a2f378114
davidortinau <david.ortinau@microsoft.com>
2021-05-23 17:13:23 -0500

    added functionality to settings and updated weather preview

--- src/WeatherTwentyOne/WeatherTwentyOne/Pages/SettingsPage.xaml.cs
+++ src/WeatherTwentyOne/WeatherTwentyOne/Pages/SettingsPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WeatherTwentyOne.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,8 @@ namespace WeatherTwentyOne.Pages
         public SettingsPage()
         {
             InitializeComponent();
+
+            BindingContext = new SettingsViewModel();
         }
 
         protected override void OnAppearing()
---
+++ src/WeatherTwentyOne/WeatherTwentyOne/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using WeatherTwentyOne.Models.WeatherTwentyOne.Models;
+using Xamarin.Forms;
+
+namespace WeatherTwentyOne.ViewModels
+{
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        private string units = "imperial";
+
+        public string Units
+        {
+            get => units;
+            set
+            {
+                units = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsImperial));
+                OnPropertyChanged(nameof(IsMetric));
+                OnPropertyChanged(nameof(IsHybrid));
+                OnPropertyChanged(nameof(Temperature));
+            }
+        }
+
+        public string Temperature
+        {
+            get
+            {
+                if(IsImperial)
+                {
+                    return "70˚F";
+                }
+                else
+                {
+                    return "21˚C";
+                }
+            }
+        }
+
+
+        public bool IsImperial
+        {
+            get {
+                return units == "imperial";
+            }
+        }
+        public bool IsMetric
+        {
+            get
+            {
+                return units == "metric";
+            }
+        }
+
+        public bool IsHybrid
+        {
+            get
+            {
+                return units == "hybrid";
+            }
+        }
+
+        public Command SelectUnits { get; set; }
+
+        public SettingsViewModel()
+        {
+            SelectUnits = new Command<string>(OnSelectUnits);
+        }
+
+        private void OnSelectUnits(string unit)
+        {
+            Units = unit;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
