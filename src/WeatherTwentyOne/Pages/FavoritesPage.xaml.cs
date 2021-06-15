f5b5af0cf183ed330a21a47f5b04a8bf58e7a44d
davidortinau <david.ortinau@microsoft.com>
2021-06-15 09:36:02 -0500

    favorites transition animation

--- src/WeatherTwentyOne/Pages/FavoritesPage.xaml.cs
+++ src/WeatherTwentyOne/Pages/FavoritesPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Maui.Controls;
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
 using WeatherTwentyOne.ViewModels;
 
 namespace WeatherTwentyOne.Pages
@@ -12,6 +15,33 @@ namespace WeatherTwentyOne.Pages
             BindingContext = new FavoritesViewModel();
 
             NavBar.ActiveTab = "Favorites";
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await Task.Delay(300);
+            TransitionIn();
+        }
+
+        async void TransitionIn()
+        {
+            foreach (var item in tiles)
+            {
+                item.FadeTo(1, 800);
+                await Task.Delay(50);
+            }
+        }
+
+        int tileCount = 0;
+        List<Frame> tiles = new List<Frame>();
+        async void OnAttached(object sender, EventArgs e)
+        {
+            
+            Frame f = (Frame)sender;
+            tiles.Add(f);
+            tileCount++;
         }
     }
 }
