543e3798a7e50b19c2c267bf96d1deff9cdbeb03
Jonathan Dick <jondick@gmail.com>
2021-09-15 13:26:36 -0400

    Fix winui

--- src/WeatherTwentyOne/Platforms/Windows/TrayService.cs
+++ src/WeatherTwentyOne/Platforms/Windows/TrayService.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WeatherTwentyOne.Services;
 
-namespace WeatherTwentyOne.WinUI
+namespace WeatherTwentyOne
 {
 	public class TrayService : ITrayService
 	{
