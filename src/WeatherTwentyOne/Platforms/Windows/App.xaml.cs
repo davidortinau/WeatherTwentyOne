b5d59009da6203b537fcc43188cecb6aad0c222e
Jonathan Dick <jondick@gmail.com>
2022-04-01 10:30:58 -0400

    Fix windows tray code for p14 (#58)

--- src/WeatherTwentyOne/Platforms/Windows/App.xaml.cs
+++ src/WeatherTwentyOne/Platforms/Windows/App.xaml.cs
@@ -21,10 +21,4 @@ public partial class App : MauiWinUIApplication
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
-    {
-        base.OnLaunched(args);
-
-        Microsoft.Maui.Essentials.Platform.OnLaunched(args);
-    }
 }
